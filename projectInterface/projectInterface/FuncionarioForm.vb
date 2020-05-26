d32c462328426f3c02f59330644e656db80d309b
Daniela <danielasplopes@ua.pt>
2020-05-26 11:06:35 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ projectInterface/projectInterface/FuncionarioForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class FuncionarioForm
+    Private Sub FuncionarioForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
--- projectInterface/projectInterface/My Project/Application.Designer.vb
+++ projectInterface/projectInterface/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.projectInterface.Form1
+            Me.MainForm = Global.projectInterface.FuncionarioForm
         End Sub
     End Class
 End Namespace
